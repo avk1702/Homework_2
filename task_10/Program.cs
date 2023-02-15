398ff1305d968a73d944d9bde83c82a8dc6773e7
avk1702 <avk1702@bk.ru>
2023-02-15 13:00:43 +0500

    Решение без метода с Random

---
+++ task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// Решение без метода с Random
+Random rnd = new Random();
+int value = rnd.Next(100,999);
+Console.WriteLine(value);
+
+int a = value / 100;
+int b = value / 10;
+int ab = b - a * 10; 
+
+System.Console.WriteLine(ab);
