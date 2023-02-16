022ade72a8b222820fcb1daf8ee749b5d45b7d48
avk1702 <avk1702@bk.ru>
2023-02-16 12:01:59 +0500

    решение с Random c методом

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -1,15 +1,13 @@
 ﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
 //обозначающую день недели, и проверяет, является ли этот день выходным.
 //6 -> да 7 -> да 1 -> нет
-// решение с Random без метода
+// решение с Random c методом
+
 Random rnd = new Random();
 int numberOfweek = rnd.Next(1,7);
 Console.WriteLine(numberOfweek);
-if (numberOfweek == 6 || numberOfweek == 7)
+bool DayOfRest(int numberOfweek)
 {
-  Console.WriteLine("Да, это выходной день ");
+  return numberOfweek == 6 || numberOfweek == 7;
 }
-else
-{
-  Console.WriteLine("Нет, это не выходной день "); 
-}
+Console.WriteLine(DayOfRest(numberOfweek));
