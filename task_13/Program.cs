a2ea9024a49fed3f79722a9bace54ed1f7766b6f
avk1702 <avk1702@bk.ru>
2023-02-16 10:53:30 +0500

    решение с вводом числа без метода

---
+++ task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 13: Напишите программу, которая выводит
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+// решение с вводом числа без метода
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int third = 0;
+while (value >= 1000)
+  {
+    value = value / 10;
+  }
+third = value % 10;
+if (value < 100)
+{
+  Console.WriteLine(" третьей цифры в числе нет ");
+}
+else
+{
+  Console.WriteLine(third);
+}
+
+
