1da8f1f377ea1c8d3305216216914794602e1073
avk1702 <avk1702@bk.ru>
2023-02-16 11:08:06 +0500

    решение с вводом числа c методом

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -2,22 +2,29 @@
 //третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
 
-// решение с вводом числа без метода
+// решение с вводом числа c методом
+
 Console.WriteLine("Введите число: ");
 int value = Convert.ToInt32(Console.ReadLine());
-int third = 0;
-while (value >= 1000)
+int ThirdDigit (int value)
+{
+  int third = 0;
+ while (value >= 1000)
   {
     value = value / 10;
   }
-third = value % 10;
+ third = value % 10;
+ return third;
+}
+
 if (value < 100)
 {
   Console.WriteLine(" третьей цифры в числе нет ");
 }
 else
 {
-  Console.WriteLine(third);
+  int res = ThirdDigit (value);
+  Console.WriteLine(res);
 }
 
 
