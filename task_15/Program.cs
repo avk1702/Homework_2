407c49069df917ba8fd8fc53fb5477ed2d13a082
avk1702 <avk1702@bk.ru>
2023-02-16 11:33:45 +0500

    решение с Random без метода

---
+++ task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да 7 -> да 1 -> нет
+// решение с Random без метода
+Random rnd = new Random();
+int numberOfweek = rnd.Next(1,7);
+Console.WriteLine(numberOfweek);
+if (numberOfweek == 6 || numberOfweek == 7)
+{
+  Console.WriteLine("Да, это выходной день ");
+}
+else
+{
+  Console.WriteLine("Нет, это не выходной день "); 
+}
